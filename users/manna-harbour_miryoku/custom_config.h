9e05676d24bb24b21ae28c31f61800760e006b2d
metziger23 <metziger23@gmail.com>
2024-11-04 15:06:07 +0300

    Added MEH_T, HYPR_T as homerow mods for outer pinky column

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -14,10 +14,10 @@
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
 ) \
 LAYOUT_split_3x6_3( \
-KC_F18,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  KC_LEFT_BRACKET, \
-KC_F17,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  KC_QUOTE, \
-KC_F16,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  KC_RIGHT_BRACKET , \
-                  K32,  K33,  K34,         K35,  K36,  K37 \
+HYPR_T(KC_F18),  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  HYPR_T(KC_LEFT_BRACKET), \
+MEH_T(KC_F17),   K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  MEH_T(KC_QUOTE), \
+KC_F16,          K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  KC_RIGHT_BRACKET , \
+                             K32,  K33,  K34,         K35,  K36,  K37 \
 )
 
 #define MIRYOKU_LAYER_BASE \
