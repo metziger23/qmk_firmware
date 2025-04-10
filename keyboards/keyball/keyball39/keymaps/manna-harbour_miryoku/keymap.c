011af96b480b240acb1b35ac885f34aafc3ff355
metziger23 <metziger23@gmail.com>
2025-04-10 09:58:10 +0300

    removed scroll mode from layer 3 (button)

--- keyboards/keyball/keyball39/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/keyball/keyball39/keymaps/manna-harbour_miryoku/keymap.c
@@ -21,12 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 #include "manna-harbour_miryoku.h"
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 3
-    keyball_set_scroll_mode(get_highest_layer(state) == 3);
-    return state;
-}
-
 #ifdef OLED_ENABLE
 
 #    include "lib/oledkit/oledkit.h"
