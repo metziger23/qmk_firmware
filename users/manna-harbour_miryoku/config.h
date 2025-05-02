7c96ad2db7fd6c7a8be300057869b5765b21b0e5
metziger23 <metziger23@gmail.com>
2025-05-02 19:41:15 +0300

    changed back TAPPING_TERM to 170

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -9,7 +9,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 250
+#define TAPPING_TERM 170
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 // #define IGNORE_MOD_TAP_INTERRUPT
