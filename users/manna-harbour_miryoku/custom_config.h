0cc9f8769a9b04419b2bce667eaef900d2c26326
mikhail <mikhail@mikhails-Mac-mini.local>
2025-05-20 09:48:35 +0300

    added combos for KC_MUTE, KC_DEL

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -100,7 +100,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 #define MIRYOKU_LAYER_MEDIA \
 TD(U_TD_BOOT),     U_NA,              U_NA,              U_NA,              U_NA,              RGB_TOG,           RGB_MOD,           RGB_HUI,           RGB_SAI,           RGB_VAI,           \
 KC_RALT,           KC_LALT,           KC_LCTL,           KC_LSFT,           KC_LGUI,           U_NU,              KC_MPRV,           KC_VOLD,           KC_VOLU,           KC_MNXT,           \
-U_NA,              KC_HYPR,           KC_MEH,            U_NA,              U_NA,              OU_AUTO,           KC_DEL,            KC_MUTE,           U_NU,              U_NU,              \
+U_NA,              KC_HYPR,           KC_MEH,            U_NA,              U_NA,              OU_AUTO,           U_NU,              U_NU,              U_NU,              U_NU,              \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_MSTP,           KC_MPLY,           KC_MUTE,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_FUN \
--- users/manna-harbour_miryoku/rules.mk
+++ users/manna-harbour_miryoku/rules.mk
@@ -8,6 +8,8 @@ TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
+COMBO_ENABLE = yes
+
 INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
 include users/manna-harbour_miryoku/custom_rules.mk
