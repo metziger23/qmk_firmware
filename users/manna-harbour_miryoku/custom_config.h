c25c637164c0009ced0e1cc427406c806073fb9e
mikhail <mikhail@mikhails-Mac-mini.local>
2025-06-03 12:17:49 +0300

    fixed LCTL for nav layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -111,10 +111,10 @@ KC_F10, KC_F1, KC_F2,  KC_F3,  KC_PAUS, U_NA,   U_NA,    U_NA,    U_NA,    U_NA,
 U_NP,   U_NP,  KC_APP, KC_SPC, KC_TAB,  U_NA,   U_NA,    U_NA,    U_NP,    U_NP
 
 #define MIRYOKU_LAYER_NAV \
-TD(U_TD_BOOT), U_NA,    U_NA,    U_NA,    U_NA,   LCTL_T(KC_B), LCTL_T(KC_D), LCTL_T(KC_U), LCTL_T(KC_F), U_NA,    \
-KC_LGUI,       KC_LALT, KC_LCTL, KC_LSFT, KC_MEH, KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT,      CW_TOGG, \
-U_NA,          U_NA,    U_NA,    U_NA,    U_NA,   KC_HOME,      KC_PGDN,      KC_PGUP,      KC_END,       KC_INS,  \
-U_NP,          U_NP,    U_NA,    U_NA,    U_NA,   KC_ENT,       KC_BSPC,      KC_DEL,       U_NP,         U_NP
+TD(U_TD_BOOT), U_NA,    U_NA,    U_NA,    U_NA,   LCTL(KC_B), LCTL(KC_D), LCTL(KC_U), LCTL(KC_F), U_NA,    \
+KC_LGUI,       KC_LALT, KC_LCTL, KC_LSFT, KC_MEH, KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    CW_TOGG, \
+U_NA,          U_NA,    U_NA,    U_NA,    U_NA,   KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     KC_INS,  \
+U_NP,          U_NP,    U_NA,    U_NA,    U_NA,   KC_ENT,     KC_BSPC,    KC_DEL,     U_NP,       U_NP
 
 #define MIRYOKU_LAYER_SYM \
 KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, U_NA,   U_NA,    U_NA,    U_NA,    TD(U_TD_BOOT), \
