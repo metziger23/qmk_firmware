4cd71d69b3d327be2c929205d54a79b90ef80cad
mikhail <mikhail@mikhails-Mac-mini.local>
2025-07-07 19:03:03 +0300

    fixed shift on home row in base layer

--- keyboards/my_charybdis/4x6/keymaps/metziger/keymap.c
+++ keyboards/my_charybdis/4x6/keymaps/metziger/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
         KC_ESC,     KC_1,         KC_2,         KC_3,         KC_4,          KC_5,        KC_6,            KC_7,          KC_8,         KC_9,         KC_0,         KC_MINS,
         MY_RU_SHCH, KC_Q,         KC_W,         KC_F,         LT(REPL,KC_P), KC_B,        KC_J,            LT(REPR,KC_L), KC_U,         KC_Y,         KC_QUOT,      LSFT(KC_SLSH),
-        MY_RU_CHE,  LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),  MEH_T(KC_G), MEH_T(KC_M),     KC_N,          LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_COMM,
+        MY_RU_CHE,  LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),  MEH_T(KC_G), MEH_T(KC_M),     LSFT_T(KC_N),  LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_COMM,
         MY_RU_ZHE,  KC_Z,         KC_X,         KC_C,         KC_D,          KC_V,        KC_K,            KC_H,          KC_COMM,      KC_DOT,       KC_SLSH,      KC_DOT,
                                   LT(MEDIA,KC_ESC), LT(NAV,KC_SPC), LT(MOUSE,KC_TAB),     LT(SYM,KC_ENT),  LT(NUM,KC_BSPC),
                                                     KC_LGUI,        KC_MEH,               LT(FUN,KC_DEL)
