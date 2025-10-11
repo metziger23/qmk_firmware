57bd03be3cac0e827053637950fb9ff9a2cee6f8
mikhail <metziger23@gmail.com>
2025-10-11 13:59:56 +0000

    use rgui instead of meh

--- keyboards/my_charybdis/4x6/keymaps/metziger/keymap.c
+++ keyboards/my_charybdis/4x6/keymaps/metziger/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
         KC_ESC,     KC_1,         KC_2,         KC_3,         KC_4,          KC_5,        KC_6,            KC_7,          KC_8,         KC_9,         KC_0,         KC_MINS,
         KC_LBRC,    KC_Q,         KC_W,         KC_F,         LT(REPL,KC_P), KC_B,        KC_J,            LT(REPR,KC_L), KC_U,         KC_Y,         KC_QUOT,      KC_RBRC,
-        KC_COLN,    LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),  MEH_T(KC_G), MEH_T(KC_M),     LSFT_T(KC_N),  LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_SCLN,
+        KC_COLN,    LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T),  RGUI_T(KC_G), RGUI_T(KC_M),     LSFT_T(KC_N),  LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_SCLN,
         KC_LPRN,    KC_Z,         KC_X,         KC_C,         KC_D,          KC_V,        KC_K,            KC_H,          KC_COMM,      KC_DOT,       KC_SLSH,      KC_RPRN,
                                   LT(MEDIA,KC_ESC), LT(NAV,KC_SPC), LT(MOUSE,KC_TAB),     LT(SYM,KC_ENT),  LT(NUM,KC_BSPC),
                                                     KC_LGUI,        KC_MEH,               LT(FUN,KC_DEL)
@@ -196,9 +196,9 @@ int get_ru_sym(int eng_sym) {
         case KC_S: return RU_EN;
         case LSFT_T(KC_T): return RU_TE;
         case KC_T: return RU_TE;
-        case MEH_T(KC_G): return RU_DE;
+        case RGUI_T(KC_G): return RU_DE;
         case KC_G: return RU_DE;
-        case MEH_T(KC_M): return RU_I;
+        case RGUI_T(KC_M): return RU_I;
         case KC_M: return RU_I;
         case LSFT_T(KC_N): return RU_A;
         case KC_N: return RU_A;
